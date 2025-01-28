bcd127c02042ed722f44769ae5cf3b095dd7d474
M Srinivasa Kalyan <m.srinivasakalyan123@gmail.com>
2025-01-28 11:20:28 +0530

    routes initial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,8 +19,18 @@
             </style>
         @endif
     </head>
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-        <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+    <body>
+        <a href="{{route('about')}}">ABOUT</a>
+        <br>
+        <a href="{{route('user', ['user_id' => 50])}}">USER PAGE</a>
+        <br>
+        <a href="{{route('blog.create')}}">create</a>
+        <br>
+        <a href="{{route('blog.edit')}}">edit</a>
+        <br>
+        <a href="{{route('blog.show')}}">show</a>
+        <br>
+        {{-- <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
             <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" alt="Laravel background" />
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
@@ -171,6 +181,6 @@
                     </footer>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return 'About page comes here';
+})->name('about');
+
+Route::get('/user/{user_id?}', function ($user_id = null) {
+    return $user_id ? $user_id : 'Not assigned';
+})->name('user');
+
+Route::group([
+    'prefix'=> 'blog', 'as'=> 'blog.'], function () {
+        Route::get('/create', function () {
+            return 'create comes here';
+        })->name('create');
+        Route::get('/edit', function () {
+            return 'edit comes here';
+        })->name('edit');
+        Route::get('/show', function () {
+            return 'show comes here';
+        })->name('show');
+    });
